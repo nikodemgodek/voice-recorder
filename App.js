43f96cf2a9a3d130b988b29b7aa0f05a509ca283
Nikodem Godek <nikodem.godek6@gmail.com>
2023-11-12 19:56:54 +0100

    Added start recording animated button

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { MotiView } from '@motify/components';
+import { Easing } from 'react-native-reanimated';
+
+import { Audio } from 'expo-av';
+import { useState } from 'react';
+
+const _size = 100;
+const _color = '#b30a';
 
 export default function App() {
+
+  const [buttonHold, setButtonHold] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Voice Recorder</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={() => setButtonHold(!buttonHold)}>
+        <View style={[styles.dot, styles.center]}>
+          {buttonHold && [...Array(3).keys()].map(index => {
+            return(
+              <MotiView 
+                from={{opacity: 1, scale: 1}} 
+                animate={{ opacity: 0, scale: 4}}
+                transition={{
+                  type: 'timing',
+                  duration: 2000,
+                  easing: Easing.out(Easing.ease),
+                  loop: true,
+                  repeatReverse: false,
+                  delay: index * 400,
+                }}
+                key={index}
+                style={[StyleSheet.absoluteFillObject, styles.dot]}
+              />
+            );
+            })}
+          <Feather name="voicemail" size={32} color='#fff' />
+        </View>
+        <Text style={{
+          marginTop: 40,
+          color: buttonHold ? "#000" : "#fff"
+        }}>Recording voice..</Text>
+      </TouchableOpacity>
+        
     </View>
   );
 }
@@ -17,4 +56,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  dot : {
+    width: _size,
+    height: _size,
+    borderRadius: _size,
+    backgroundColor: _color,
+  },
+
+  center : {
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
